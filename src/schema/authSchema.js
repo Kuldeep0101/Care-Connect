b72f61b6d1c6fa59e764dc5056db6648434e4d4b
Kuldeep0101 <kuldeepsingh44682@gmail.com>
2025-09-07 08:39:35 +0530

    Added Zod schema validation

--- src/schema/authSchema.js
+++ src/schema/authSchema.js
@@ -1,13 +1,15 @@
 const z = require("zod");
 
-const userSchema = z.object({
+const signupSchema = z.object({
   name: z.string().min(4, { error: "first Name must be 4 charachters long" }),
-  email: z.email(),
-  password: z.string().min(6, { error: "password must be 6 charechters long" }),
-  role: z.enum(["patient", "doctor"]),
+  email: z.email("Invalid Email"),
+  password: z.string().min(6, "Password too short"),
+  role: z.enum(["patient", "doctor"], { message: "invalid role" }),
 });
 
 const loginSchema = z.object({
-    email: z.email(),
-    password: z.string().min(6)
-})
+  email: z.email(),
+  password: z.string().min(6, "Password too Short"),
+});
+
+module.exports = { signupSchema, loginSchema };
